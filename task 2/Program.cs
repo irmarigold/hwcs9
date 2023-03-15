7aad8fa52ba1559f4c29ee4f2c4c9b98f3c2a96f
Irene Zernova <irenezernova@gmail.com>
2023-03-15 12:54:02 +0300

    second task is already done

---
+++ task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿int akkerman(int m, int n)
+{
+    if (m == 0)
+        return (n+1);
+    if (n == 0)
+        return akkerman(m-1,1);
+    return akkerman(m-1, akkerman(m,n-1));
+}
+
+Console.Clear();
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(akkerman(m,n));
