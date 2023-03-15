0d5e6fc4355c5c9c2a65ed3fcb2135e0fbc2fb0c
Irene Zernova <irenezernova@gmail.com>
2023-03-15 12:39:04 +0300

    first

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int PlusElem(int n, int m)
+{
+    if (n == m)
+        return m;
+    return n+PlusElem(n-1,m);
+}
+
+Console.Clear();
+Console.Write("Input M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("nput N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(PlusElem(n,m));
